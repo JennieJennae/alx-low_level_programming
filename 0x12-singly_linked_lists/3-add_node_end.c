c2221606abcb2c0f0f0e2ce4ee3834e496bee5f6
JennieJennae <chinonyejennie@gmail.com>
2023-05-01 06:05:38 -0700

    3-add_node_end.c updated

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,18 +1,50 @@
 #include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 /**
- * free_list - responsible for freeing a linked list
- * @head: freed list_t
- */
-void free_list(list_t *head)
+* _strlen - string length finder
+* @str: string
+* Return: string length
+*/
+unsigned int _strlen(char *str)
 {
-	list_t *temp;
+	unsigned int i;
 
-	while (head)
+	for (i = 0; str[i]; i++);
+	return (i);
+}
+
+/**
+* add_node_end - adds a new node
+* @head: double pointer 
+* @str: string
+* Return: pointer
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
+	
+	if (str == NULL)
+	return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
-	temp = head->next;
-	free(head->str);
-	free(head);
-	head = temp;
+		free(new);
+		return (NULL);
 	}
+	new->len = _strlen(new->str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next);
+	tmp = tmp->next;
+	tmp->next = new;
+	return (new);
 }
--- 0x12-singly_linked_lists/3-main.c
+++ 0x12-singly_linked_lists/3-main.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
  * main - check the code
  *
@@ -14,15 +13,15 @@ int main(void)
 
 	head = NULL;
 	add_node_end(&head, "Anne");
-	add_node_end(&head, "Colton");	
+	add_node_end(&head, "Colton");
 	add_node_end(&head, "Corbin");
-	add_node_end(&head, "Daniel");			
+	add_node_end(&head, "Daniel");
 	add_node_end(&head, "Danton");
 	add_node_end(&head, "David");
 	add_node_end(&head, "Gary");
 	add_node_end(&head, "Holden");
 	add_node_end(&head, "Ian");
-	add_node_end(&head, "Ian");	
+	add_node_end(&head, "Ian");
 	add_node_end(&head, "Jay");
 	add_node_end(&head, "Jennie");
 	add_node_end(&head, "Jimmy");
