fdaba1b165e13b36d8e24ae00d7e9a9d34c01c6b
JennieJennae <you@example.com>
2023-06-22 15:28:49 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,9 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
+ * sum_them_all - finds the sum of all parameters
+ * @n: number of paramters 
+ * @...: a configurable amount of input parameters for the total.
  *
  * Return: If n == 0 - 0.
  *         Otherwise - the sum of all parameters.
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -3,3 +3,4 @@ Tasks
 1. To be is to be the value of a variable
 2. One woman's constant is another woman's variable
 3. To be is a to be the value of a variable
+end of task
