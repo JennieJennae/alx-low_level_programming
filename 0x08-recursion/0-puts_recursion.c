05354179a474903630c229eff39e75a4ac1ded2d
JennieJennae <122867649+JennieJennae@users.noreply.github.com>
2023-06-22 09:50:25 +0100

    0-puts_recursion.c updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,25 @@
 #include "main.h"
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * This function prints a string character by character in recursive fashion till the string's conclusion.
+ * it is alike with standard library function puts().
+ *
+ * @param str: The string that needs to be printed
+ * @return: void
  */
 
 void _puts_recursion(char *s)
 {
+	 // Confirm that the current character is not null
 	if (*s)
 	{
+		//print the current character
 		_putchar(*s);
+		//recursively print function with the next character
 		_puts_recursion(s + 1);
 	}
-
 	else
+	{
+		//print a new line character
 		_putchar('\n');
-
+	}
 }
