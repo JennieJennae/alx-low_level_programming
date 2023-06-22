da770c0ec1a639590703f7067562b0e0df841e1f
JennieJennae <you@example.com>
2023-06-22 16:35:46 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
+ * main - Prints simple operations results.
+ * @argc: number of arguments supplied.
  * @argv: An array of pointers to the arguments.*
  * Return: Always 0.
  */
