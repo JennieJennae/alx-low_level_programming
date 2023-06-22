75aee7c5f558c751e67d8dd45e6ccaf6e9ada4c7
JennieJennae <you@example.com>
2023-06-22 16:21:11 -0700

    updated

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * array_iterator - prints each array element 
+ * array_iterator - prints each array element
  * on a new line
  * @array: array
  * @size: how many eleent to be  printed
