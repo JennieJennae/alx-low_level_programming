f9b473f4ee42563b89a0ddc516fd2203ddba3bf0
JennieJennae <you@example.com>
2023-06-26 03:03:34 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,14 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
+ * struct dog - the dog's info
  * @name: First member
  * @age: Second member
  * @owner: Third member
  *
  * Description: Longer description
  */
+
 struct dog
 {
 	char *name;
