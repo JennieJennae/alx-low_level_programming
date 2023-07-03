bb977504b1617b62a772a369a1a4b114617ade10
JennieJennae <you@example.com>
2023-07-03 16:54:31 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+
 /**
- * add_node - adds a new node to a linked file
+ * add_node - adds a new node
  * @head: double pointer to the list_t list
- * @str: new string to add in the node
- * Return: the address of the new element, or NULL if it fails
+ * @str: new string
+ * Return: NULL if it fails
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
--- 0x12-singly_linked_lists/2-main.c
+++ 0x12-singly_linked_lists/2-main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+
 #include "lists.h"
 /**
  * add_node_end - new node is added to the end of a linked list
